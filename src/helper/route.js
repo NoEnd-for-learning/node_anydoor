8b9e29997dc8d19807a3c1d9f23e57441067cea5
xrtShu <xurongtian@co-mall.com>
2019-01-27 01:52:42 +0800

    t-[190127]-[新增range 范围请求，cmd 命令行：curl -r 0-10 -i http://127.0.0.1:9000/app.js 进行测试]-[hygge]

--- src/helper/route.js
+++ src/helper/route.js
@@ -7,6 +7,7 @@ const fs_stat = promisify(fs.stat);
 const fs_readdir = promisify(fs.readdir);
 const conf = require('../config/default-config');
 const compress = require('../helper/compress');
+const range = require('../helper/range');
 
 const tplPath = path.join(__dirname, '../template/dir.tpl');
 // 同步读取，因为只执行一次，并且后面的代码依赖到它
@@ -20,7 +21,18 @@ module.exports = async function (req, res, filePath) {
             res.statusCode = 200;
             res.setHeader('Content-Type', mime.lookup(filePath));
 
-            let rs = fs.createReadStream(filePath);
+            // range
+            let rs;
+            const { code, start, end } = range(stats.size, req, res);
+            if (code === 200) {
+                rs = fs.createReadStream(filePath);
+            } else {
+                rs = fs.createReadStream(filePath, {
+                    start, end
+                });
+            }
+
+            // compress
             if (filePath.match(conf.compress)) {
                 rs = compress(rs, req, res);
             }
